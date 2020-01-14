7d42d30e366b8e5dbf9c135d307b03dba29ad44a
Marcelo Martinez <marcelomst1@gmail.com>
2020-01-14 13:13:26 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,20 @@
 //define functions here
-
+function getIt(){
+  alert("Hey!");
+}
+function frameIt(){
+    $("img").addClass("tasty"); 
+}
+function pressIt(){
+  alert("You has pressed the G key");
+}
 $(document).ready(function(){
-
 // call functions here
-
+  $("p").on("click", getIt);
+  $("img").on("load", frameIt);
+  $("").on("keydown", function(event){
+     if (event.which == 71){
+      pressIt();
+     }
+     });
 });
