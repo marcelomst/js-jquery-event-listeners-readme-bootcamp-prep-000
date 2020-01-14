b4fa01e4abff25faa541838b87477e5925e0debc
Marcelo Martinez <marcelomst1@gmail.com>
2020-01-14 13:25:02 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,20 +1,20 @@
 //define functions here
 function getIt(){
-  alert("Hey!");
+  $("p").on("click", function () {alert("Hey!");});
 }
 function frameIt(){
-    $("img").addClass("tasty"); 
+    $("img").on("load", function() {addClass("tasty")}); 
 }
 function pressIt(){
-  alert("You has pressed the G key");
+  $("form input").on("keydown", function(key){
+     if (key.which == 71){
+       alert("You has pressed the G key");
+     }
+     });
 }
 $(document).ready(function(){
 // call functions here
-
-  $("img").on("load", frameIt);
-  $("").on("keydown", function(key){
-     if (key.which == 71){
-      pressIt();
-     }
-     });
+  getIt();
+  frameIt();
+  pressIt();
 });
